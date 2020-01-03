704b19ea8a2dd6b3517cf534a14f7d2797941872
matt-peck-masheen <matt.peck@masheen.ai>
2020-01-03 17:34:18 -0500

    Added reading list, sort, data, and cleaned up styles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import moment from "moment";
+import readingList from "./data/readingList.json";
 
-function App() {
+const formatDate = date => moment(date).format("MMM D");
+
+const ReadingListItem = props => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="book">
+      {props.title}
+      {props.start && (
+        <span className="start">Started - {formatDate(props.start)}</span>
+      )}
     </div>
   );
-}
+};
+
+const App = () => {
+  const sortedReadingList = readingList.sort((a, b) => {
+    const momentA = new moment(a.start);
+    const momentB = new moment(b.start);
+
+    if (momentA.isValid() && momentB.isValid()) {
+      return momentA.diff(momentB);
+    }
+
+    if (momentA.isValid() && !momentB.isValid()) {
+      return -1;
+    }
+
+    if (!momentA.isValid() && momentB.isValid()) {
+      return 1;
+    }
+
+    if (!momentA.isValid() && !momentB.isValid()) {
+      return 0;
+    }
+  });
+
+  return (
+    <header className="App-header">
+      <h1>2020 Reading List</h1>
+      {sortedReadingList.map(item => (
+        <ReadingListItem
+          key={item.title}
+          title={item.title}
+          start={item.start}
+        />
+      ))}
+    </header>
+  );
+};
 
 export default App;
