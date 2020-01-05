9396f03e9f43e0e887d2167eb8e4b87790c8a51a
matt-peck-masheen <matt.peck@masheen.ai>
2020-01-04 20:26:08 -0500

    Adding goals fetch from ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import moment from "moment";
 import readingList from "./data/readingList.json";
 
@@ -15,40 +15,47 @@ const ReadingListItem = props => {
   );
 };
 
-const App = () => {
-  const sortedReadingList = readingList.sort((a, b) => {
-    const momentA = new moment(a.start);
-    const momentB = new moment(b.start);
+class App extends Component {
+  async componentDidMount() {
+    const goals = await fetch("/.netlify/functions/getGoals");
+    console.log({ goals });
+  }
 
-    if (momentA.isValid() && momentB.isValid()) {
-      return momentA.diff(momentB);
-    }
+  render() {
+    const sortedReadingList = readingList.sort((a, b) => {
+      const momentA = new moment(a.start);
+      const momentB = new moment(b.start);
 
-    if (momentA.isValid() && !momentB.isValid()) {
-      return -1;
-    }
+      if (momentA.isValid() && momentB.isValid()) {
+        return momentA.diff(momentB);
+      }
 
-    if (!momentA.isValid() && momentB.isValid()) {
-      return 1;
-    }
+      if (momentA.isValid() && !momentB.isValid()) {
+        return -1;
+      }
 
-    if (!momentA.isValid() && !momentB.isValid()) {
-      return 0;
-    }
-  });
+      if (!momentA.isValid() && momentB.isValid()) {
+        return 1;
+      }
 
-  return (
-    <header className="App-header">
-      <h1>2020 Reading List</h1>
-      {sortedReadingList.map(item => (
-        <ReadingListItem
-          key={item.title}
-          title={item.title}
-          start={item.start}
-        />
-      ))}
-    </header>
-  );
-};
+      if (!momentA.isValid() && !momentB.isValid()) {
+        return 0;
+      }
+    });
+
+    return (
+      <header className="App-header">
+        <h1>2020 Reading List</h1>
+        {sortedReadingList.map(item => (
+          <ReadingListItem
+            key={item.title}
+            title={item.title}
+            start={item.start}
+          />
+        ))}
+      </header>
+    );
+  }
+}
 
 export default App;
