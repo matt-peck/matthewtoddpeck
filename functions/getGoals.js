5595d16ae2322c9f5d75e91893265cb322d5393a
matt-peck-masheen <matt.peck@masheen.ai>
2020-01-04 19:50:36 -0500

    Added clickup tasks fetch functino

--- functions/getGoals.js
+++ functions/getGoals.js
@@ -1,8 +1,30 @@
 const { CU_API_TOKEN } = process.env;
 
+const fetchGoals = async () => {
+  const config = {
+    method: "GET",
+    // mode: 'cors', // no-cors, *cors, same-origin
+    // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    // credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: CU_API_TOKEN
+    }
+    // redirect: 'follow', // manual, *follow, error
+    // referrerPolicy: 'no-referrer', // no-referrer, *client
+    // body: JSON.stringify(data) // body data type must match "Content-Type" header
+  };
+  return fetch(
+    "https://api.clickup.com/api/v2/list/10649956/task?archived=false",
+    config
+  ).then(response => response.json());
+};
+
 exports.handler = async (event, context) => {
+  const goals = await fetchGoals();
+
   return {
     statusCode: 200,
-    body: "hello world"
+    body: goals
   };
 };
