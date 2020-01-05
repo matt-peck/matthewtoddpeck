be7f3f627d43169805d8f730f380efa513754d62
matt-peck-masheen <matt.peck@masheen.ai>
2020-01-04 20:49:37 -0500

    Added some logging to function

--- functions/getGoals.js
+++ functions/getGoals.js
@@ -3,6 +3,8 @@ const axios = require("axios");
 const { CU_API_TOKEN } = process.env;
 
 const fetchGoals = () => {
+  console.log("fetchGoals called!");
+
   const config = {
     method: "GET",
     mode: "cors", // no-cors, *cors, same-origin
@@ -20,13 +22,18 @@ const fetchGoals = () => {
 
 exports.handler = async (event, context) => {
   try {
+    console.log("trying fetchGoals()...");
     const goals = await fetchGoals();
+
+    console.log("goals fetched!", goals);
 
     return {
       statusCode: 200,
       body: JSON.stringify(goals)
     };
   } catch (err) {
+    console.log("error", err);
+
     return {
       statusCode: 400,
       body: `Error: ${err}`
